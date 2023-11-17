5ef166528e3da7772728a2940002cd0dfeb25d9f
Gabriel Todt <gabrielrtodt@gmail.com>
2023-11-17 00:42:55 -0300

    feat: Huffman hashMap implementation

--- main/java/algorithms/Huffman/Huffman.java
+++ main/java/algorithms/Huffman/Huffman.java
@@ -10,12 +10,18 @@ public class Huffman {
     private final RandomAccessFile source;
     private HashMap<Vector, Integer> hashMap;
 
-
     public Huffman(RandomAccessFile source) {
         this.source = source;
         this.hashMap = new HashMap<Vector, Integer>();
     }
 
+    /**
+     * Creates a dictionary based on the specified block size from the source data.
+     * The dictionary is represented using a HashMap where keys are vectors of bytes.
+     *
+     * @param blockSize The size of each block to be read from the source.
+     * @throws IOException If an I/O error occurs while reading from the source.
+     */
     public void createDictionary(int blockSize) throws IOException {
         long pos = source.getFilePointer();
         source.seek(0);
@@ -28,11 +34,15 @@ public class Huffman {
             }
             else {
                 hashMap.put(tmp, 1);
-                System.out.println("novo");
             }
         }
         source.seek(pos);
     }
+
+    /**
+     * Prints the contents of the HashMap, displaying each vector and its associated count.
+     * Additionally, it prints the total number of unique vectors in the HashMap.
+     */
     public void printMap(){
         for(Vector v: hashMap.keySet()){
             System.out.println(Arrays.toString(v.getArray())+ " --> "+hashMap.get(v));
